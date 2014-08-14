463c23a043a3837b14dd9e126f0eb4130257362d
clattner <chris.lattner@live.com>
2014-08-14 08:25:29 -0500

    testcommit

---
+++ CrowdLuminary02/CrowdLuminary02/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CrowdLuminary02.MvcApplication" Language="C#" %>
---
+++ CrowdLuminary02/CrowdLuminary02/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CrowdLuminary02.Startup))]
+namespace CrowdLuminary02
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
