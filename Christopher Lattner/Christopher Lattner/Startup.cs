febf84faad2e4ba04a50a4be073820dd2290f326
clattner <chris.lattner@live.com>
2014-08-14 08:30:49 -0500

    test05

---
+++ Christopher Lattner/Christopher Lattner/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Christopher_Lattner.Startup))]
+namespace Christopher_Lattner
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
